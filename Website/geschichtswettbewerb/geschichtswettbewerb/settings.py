58920979f22b0ec6113c776c086b1719045d15c4
Richard Albrecht <r_albr03@uni-muenster.de>
2021-06-03 17:41:35 +0200

    alter settings for deployment

--- Website/geschichtswettbewerb/geschichtswettbewerb/settings.py
+++ Website/geschichtswettbewerb/geschichtswettbewerb/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'd%)-s%ioiq(iwt=272nlguu-2o9dtk$jtt0qnwqgmn@)d05+vc'
+#SECRET_KEY = 'd%)-s%ioiq(iwt=272nlguu-2o9dtk$jtt0qnwqgmn@)d05+vc'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'd%)-s%ioiq(iwt=272nlguu-2o9dtk$jtt0qnwqgmn@)d05+vc')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = []
 
