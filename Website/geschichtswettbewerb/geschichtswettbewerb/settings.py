7ce07cf591afe8fc5538000d56ac909ac8ca1da5
Richard Albrecht <r_albr03@uni-muenster.de>
2021-06-16 21:40:59 +0200

    set static_root

--- Website/geschichtswettbewerb/geschichtswettbewerb/settings.py
+++ Website/geschichtswettbewerb/geschichtswettbewerb/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'd%)-s%ioiq(iwt=272nlguu-2o9dtk
 #DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['d-3110s70.wiwi.uni-muenster.de', 'localhost']
 
 
 # Application definition
@@ -131,3 +131,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
