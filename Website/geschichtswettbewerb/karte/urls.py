8a27ff42c3d978b9eccbfaae105c90e4233e0f82
Richard Albrecht <r_albr03@uni-muenster.de>
2021-09-16 13:23:16 +0200

    change url structure

--- Website/geschichtswettbewerb/karte/urls.py
+++ Website/geschichtswettbewerb/karte/urls.py
@@ -6,11 +6,11 @@ from django.views.generic import RedirectView
 app_name = 'karte'
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('karte/', PlaceMapView.as_view(), name='karte'),
+    #path('', views.index, name='index'),
+    path('', PlaceMapView.as_view(), name='karte'),
     path('orte/', views.PlaceListView.as_view(), name='orte'),
     path('beitraege/', views.SubmissionFilterView.as_view(), name='beitraege'),
-    path('karte/<int:pk>', views.PlaceDetailView.as_view(), name='ort-detail'),
+    path('<int:pk>', views.PlaceDetailView.as_view(), name='ort-detail'),
     path('orte/<int:pk>', views.PlaceDetailView.as_view(), name='ort-detail'),
     path('beitraege/<int:pk>', views.SubmissionDetailView.as_view(), name='submission-detail'),
     path('wettbewerbe',  views.CompetitionListView.as_view(), name='wettbewerbe'),
